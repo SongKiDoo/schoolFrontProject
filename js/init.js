58140dac2c5374f46403e8eebb996903b169ffa0
KiDooSong <only3ss@smartcon-metaps.com>
2018-10-28 18:30:47 +0900

    [add] 상세 카테고리 추가

--- js/init.js
+++ js/init.js
@@ -7,11 +7,16 @@
         $(".dCateCardDiv").slick({
 
             // normal options...
+            // 노출 개수
+            slidesToShow: 5,
+            // 스크롤시 이동될 개수
+            slidesToScroll: 1,
+            adaptiveHeight: true,
             infinite: false,
-
             // the magic
-            responsive: [{
 
+            // 해상도별 설정
+            responsive: [{
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 4,
@@ -32,17 +37,23 @@
                 settings: "unslick" // destroys slick
 
             }],
-            arrows : false
-            // appendArrows : $('.fa-chevron-left')
+            // arrows : false
 
 
         });
 
-        $('.fa-chevron-left').on('click', function(){
-            $('#carCate').slick("slickPrev");
+        // 상세카테고리 before 리스너
+        $('.fa-chevron-left').on('click', function(event){
+            // 부모의 ID로 구분해 화면 이동처리
+            var contextId = $(this).parent().parent().attr('id').replace('Arrow', '');
+            $('#' + contextId).slick("slickPrev");
         });
-        $('.fa-chevron-right').on('click', function(){
-            $('#carCate').slick("slickNext");
+
+        // 상세카테고리 after 리스너
+        $('.fa-chevron-right').on('click', function(event){
+            // 부모의 ID로 구분해 화면 이동처리
+            var contextId = $(this).parent().parent().attr('id').replace('Arrow', '');
+            $('#' + contextId).slick("slickNext");
         });
 
 
