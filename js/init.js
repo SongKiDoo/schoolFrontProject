6c102e7f70c5d2218bae21c9ed97c63249506c03
KiDooSong <only3ss@smartcon-metaps.com>
2018-10-28 15:54:47 +0900

    [add] 상세 카테고리 추가중 [add] slick slider

--- js/init.js
+++ js/init.js
@@ -1,8 +1,50 @@
-(function($){
-  $(function(){
+(function ($) {
+    $(function () {
 
-    $('.sidenav').sidenav();
-    $('.parallax').parallax();
+        $('.sidenav').sidenav();
+        $('.parallax').parallax();
 
-  }); // end of document ready
+        $(".dCateCardDiv").slick({
+
+            // normal options...
+            infinite: false,
+
+            // the magic
+            responsive: [{
+
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4,
+                    infinite: true
+                }
+
+            }, {
+
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    dots: true
+                }
+
+            }, {
+
+                breakpoint: 300,
+                settings: "unslick" // destroys slick
+
+            }],
+            arrows : false
+            // appendArrows : $('.fa-chevron-left')
+
+
+        });
+
+        $('.fa-chevron-left').on('click', function(){
+            $('#carCate').slick("slickPrev");
+        });
+        $('.fa-chevron-right').on('click', function(){
+            $('#carCate').slick("slickNext");
+        });
+
+
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
