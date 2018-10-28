84eb573a5fb0ff11a7a5cbf486bd00103a26a7c7
KiDooSong <only3ss@smartcon-metaps.com>
2018-10-29 00:49:32 +0900

    [add] 카드박스 추가

---
+++ js/css-action.js
@@ -0,0 +1,64 @@
+(function() {
+    var totalSecond = 0;
+    var remainingIntervalFlag = false;
+
+    // 자식 아이템 hover 상단 헤더 투명도 조절
+    $('.cardBoxItem').hover(function () {
+        $(this).parent().parent().children('.cardBoxHead').css('opacity', 1);
+    }, function () {
+        $(this).parent().parent().children('.cardBoxHead').css('opacity', 0.8);
+    });
+
+    // 아이템내 이미지 hover 처리
+    $('.cardBoxItem .imgDiv').hover(function () {
+        var remainingText = $(this).children('.remainingDateText');
+        var parsent = $(this).parent();
+        var remaininTime = parsent.attr('data-value');
+
+        if(remaininTime) {
+            // D이면 날짜, 아니면 데이터 포맷 형식
+            if(remaininTime[0] === 'D') {
+                remainingText.html(remaininTime);
+            } else {
+                remainingText.html(remaininTime);
+                var dateFormat = remaininTime.split(':');
+                var hour = Number(dateFormat[0]) * 3600;
+                var min = Number(dateFormat[1]) * 60;
+                var second = Number(dateFormat[2]);
+                totalSecond = hour+min+second;
+
+                // 1초씩 반복, 초단위 계산하여 데이터 포맷 만든 후 뷰에 노출
+                remainingIntervalFlag = setInterval(function () {
+                    if(remainingIntervalFlag) {
+                        totalSecond -= 1;
+
+                        //소수점 제거
+                        min = parseInt(totalSecond/60);
+                        hour = parseInt(min/60);
+                        second = totalSecond % 60;
+                        min = min % 60;
+
+                        // 2자리수 만들기
+                        hour = hour < 10 ? '0' + hour : hour;
+                        min = min < 10 ? '0' + min : min;
+                        second = second < 10 ? '0' + second : second;
+                        var viewData = hour+':'+min+':'+second;
+
+                        // 데이터 저장
+                        parsent.attr('data-value', viewData);
+                        // 뷰 노출
+                        remainingText.html(viewData);
+                    }
+                },1000);
+            }
+            // hover 글씨 노출처리
+            remainingText.css('visibility', 'visible');
+        }
+    }, function () {
+        var remainingText = $(this).children('.remainingDateText');
+        remainingText.css('visibility', 'hidden');
+        // hover out 반복 제거
+        clearInterval(remainingIntervalFlag);
+    });
+
+})();
--- js/init.js
+++ js/init.js
@@ -1,3 +1,4 @@
+var module;
 (function ($) {
     $(function () {
 
